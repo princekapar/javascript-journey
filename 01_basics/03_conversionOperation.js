37614d0674211d509da13dadc8937e95582e57ec
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-11 01:40:41 +0530

    operations discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,47 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
+// ************************Operations***************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+
+
+let str1 = "hello"
+let str2 = " Prince"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+//console.log(+true);
+
+//console.log(+""); //just to learn
+
+
+let gameCounter = 100;
+
+++gameCounter;
+
+console.log(gameCounter);
