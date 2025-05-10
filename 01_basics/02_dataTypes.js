4e4d8416d3f50173c360fc38a7b7ad22093abdb3
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-11 00:54:27 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") // we are using node.js, not browser
+
+
+console.log(3 + 3); //code readability should be high
+console.log("Prince")
+
+let name = "Prince"
+let age = 13
+let isLoggedIn = false
+
+
+    // number => 2 to power 53
+    //bigint
+    // string => ""
+    // boolean => ture / false
+    //null => standalone value // object
+    // undefine => empty
+    // symbol => unique
+
+
+    //object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
