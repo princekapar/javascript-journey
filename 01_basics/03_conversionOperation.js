fee229715cc8a93d5e627b7ea578b0367d7be8e3
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-11 01:18:42 +0530

    conversion datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueNumber = Number(score)
+//console.log(typeof valueNumber);
+//console.log(valueNumber)
+
+
+// "33" =>33
+//"33dsg" => NaN
+//ture = 1 and false = 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 = true
+//0 = false
+//"hostinger"= true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber);
