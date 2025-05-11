d6fca81a4989ce17b839ab737980cb368878d2fb
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-11 12:06:26 +0530

    stack and heap memory concept

---
+++ 01_basics/stack_heap_memory.js
@@ -0,0 +1,26 @@
+
+//Stack (Primitive ), Heap(Non-Primitve)
+
+
+
+let myStoryName = "Tellyoustory";
+
+let anotherName = "tellyouname";
+
+anotherName = "Doyoujob"
+
+console.log(myStoryName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@xlb"
+} 
+
+let user2 = userOne;
+
+user2.email = "princekapar@gamil.com";
+
+console.log(userOne.email);
+console.log(user2.email);
