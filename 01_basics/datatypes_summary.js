55942ce1ce521471fd860ddf515bd395be266638
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-11 10:17:20 +0530

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,45 @@
+
+// Primitive
+
+// types: string, Number, Boolean, Undefine, Null, symbol, BigInt
+
+
+const score = 100;
+const scoreValue = 100.4;
+
+const isLoggedIn = false;
+const ousideTemp = null;
+
+let userEmail;
+
+const id = Symbol("234");
+const anotherId = Symbol("234")
+
+console.log(id === anotherId)
+
+const bigNumber = 11111111111
+
+// Referance (Non-primitive)
+
+//Array, Objects, Functions,
+
+
+const heros = ["Shaktiman", "Naagraj", "Doga,"];
+
+let myObj = {
+    name: "Prince",
+    age:23
+}
+
+const myfunction = function () {
+    console.log("Hello JavaScript");
+// function objects
+}
+
+console.log(typeof heros);
+console.log(typeof myObj)
+console.log(typeof bigNumber)
+console.log(typeof ousideTemp)
+console.log(typeof myfunction)
+console.log(typeof isLoggedIn)
+console.log(typeof id)
