20ac0bc8fde174e5aeac4d5ebd4858bbeda9c94f
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-20 22:32:23 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+//switch
+
+
+const month = "April"
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("Februrary");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case "April":
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("invalid")
+        break;
+}
