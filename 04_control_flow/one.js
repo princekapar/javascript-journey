7cdc98a34407ce7915087c23537cec44b5716bbb
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-20 22:24:29 +0530

    if, if else, else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,66 @@
+
+//if
+
+// const isUserloggedIn = true
+
+// if (4 != 8) {
+//     console.log("executed")
+// }
+
+// const temperature = 55
+
+// if (temperature < 40) {
+//     console.log("less than 40");
+// }
+// else { console.log("temperature is greater than 40")}
+
+//<,>,<=,>=,==,!=,===
+
+
+
+
+
+// const score = 300
+
+// if (score > 200) {
+//     let power = "fly";
+//     console.log(`user power: ${power}`);
+// }
+
+//  console.log(`user power: ${power}`);
+
+
+
+
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test"),console.log("test2")//don't write code like this
+
+// if (balance < 500) {
+//     console.log("less than 500 ")
+// }
+// else if (balance < 750) {
+//     console.log("less than 750")
+// }
+// else if (balance < 900) {
+//     console.log("less than 900")
+// }
+// else {
+//     console.log("less than 1200")
+// }
+
+
+const userLggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLggedIn && debitCard) {
+    console.log("Allow to buy whatever they want buy")
+}
+
+if (loggedInFromEmail || loggedInFromEmail) {
+    console.log("User logged in")
+}
