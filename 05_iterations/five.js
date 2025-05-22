fd9108ceff2d000fb2a16c24422c603928c36770
PRINCE-KAPAR <princekapar02@gmail.com>
2025-05-22 22:10:17 +0530

    for in loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,23 @@
+
+const coding = ["js", "rb", "py", "java", "cpp"]
+
+coding.forEach(function (item) {
+ //   console.log(item);
+    
+})
+
+coding.forEach((item) => {
+   // console.log(item);  
+})
+
+function printMe(item) {
+    console.log(item);
+    
+}
+
+//coding.forEach(printMe)
+
+
+coding.forEach((val, index, arr) => {
+   // console.log(val, index, arr);
+})
