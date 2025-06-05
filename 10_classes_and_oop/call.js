2b74177a884c9115a353d1b70d926bbecd801aac
PRINCE-KAPAR <princekapar02@gmail.com>
2025-06-05 22:22:03 +0530

    call and this

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,18 @@
+function setUsername(username) {
+    //complex DB calls 
+    this.username = username
+    console.log("called")
+}
+
+
+function createUser(username, email, password) {
+    setUsername.call(this, username)
+
+
+    this.email = email
+    this.password = password
+    
+}
+
+const chai = new createUser("chai", "prince@gamil.co", "123")
+console.log(chai)
